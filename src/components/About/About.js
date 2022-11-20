25dc84a365d8cde14307000d04cbb4c6fa926dac
Nitzan Cohen <nitzancohen@hotmail.com>
2022-11-20 10:09:21 +0200

    Update About section.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,10 +9,16 @@ function About(props) {
             <div className="about__description">
                 <h2 className="about__title">Nitzan Cohen</h2>
                 <p className="about__text">
-                    Hello, I am Nitzan and this is a website the I built as my final project of Full Stack Web Development course in Practicum program by Yandex and Masterschool.
+                    Hello, I am Nitzan, I built this SPA (Single Page Application) as my final project of Full Stack Web Development course in Practicum program by Yandex and Masterschool.
                 </p>
                 <p className="about__text">
-                    I added some important tools for my tool box, like HTML, CSS, JS, React, MongoDB and more. I really love to develop technology tools, and I have some ideas for personal projects.
+                    I used in this project with some technologies tools which I learned in the course. Firstly, the purpose of this website is to give the users to search news articles from the whole internet, and registered users can save articles for later reading.
+                </p>
+                <p className="about__text">
+                    On the technical side, I built this website with HTML and CSS according to design from Figma. I used with React.js to build the framework and JSX to create the functionality of all the features on the website.
+                </p>
+                <p className="about__text">
+                    I'm working with NewsApi to search articles from anothers websites. I built a server with Express.js that keep the registered users and their saved articles, with database that I created with MongoDB. And I deployed all of that to Linux Virtual Machine on Google Cloud.
                 </p>
             </div>
         </section>
